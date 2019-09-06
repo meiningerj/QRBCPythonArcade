e72ec54b8d1e7307223536caf62ad7004846d2b6
Jamie Meininger <j.p.meininger@se15.qmul.ac.uk>
2019-09-06 22:27:44 +0100

    Create PythonArcade.py

---
+++ PythonArcade.py
@@ -0,0 +1,13 @@
+import arcade
+import os
+
+arcade.open_window(600, 600, "ArcadeIntroduction")
+
+arcade.start_render()
+
+#this is where you type all the code you want to use to draw
+#for example arcade.draw_rectangle_filled(..) or arcade.draw_ellipse_filled()
+
+arcade.finish_render()
+
+arcade.run()
